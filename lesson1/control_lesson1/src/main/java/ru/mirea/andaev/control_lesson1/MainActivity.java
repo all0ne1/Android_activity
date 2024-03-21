93e16c17c9acd7b6690147ac3cbeba93fe997f76
all0neee <Andaev.1@yandex.ru>
2024-03-21 21:11:14 +0300

    lesson_1

---
+++ lesson1/control_lesson1/src/main/java/ru/mirea/andaev/control_lesson1/MainActivity.java
@@ -0,0 +1,29 @@
+package ru.mirea.andaev.control_lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+
+        Button button = findViewById(R.id.button);
+        button.setText("MIREAButton");
+
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
+
+    }
+}
